46eaaf4ad2b1d96a9c7c7ec50f7e1501072511d8
Shawn Schwartz <shawnschwartz@ucla.edu>
2021-06-08 01:39:10 -0700

    update helper function to grab all relevant files from docker container run

--- get_sashimi.cmd
+++ get_sashimi.cmd
@@ -2,4 +2,7 @@
 docker ps -alq > tmp_id.txt
 set /p msg= < tmp_id.txt
 docker cp "%msg%":/app/_outputs/. ./_outputs
+docker cp "%msg%":/app/_models/. ./_models
+docker cp "%msg%":/app/_logs/. ./_logs
+docker cp "%msg%":/app/sashimi/logs/. ./sashimi/logs
 del tmp_id.txt
--- get_sashimi.sh
+++ get_sashimi.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 tmpid="$(docker ps -alq)"
-docker cp tmpid:/app/_outputs/. ./_outputs
+docker cp $tmpid:/app/_outputs/. ./_outputs
+docker cp $tmpid:/app/_outputs/. ./_outputs
+docker cp $tmpid:/app/_models/. ./_models
+docker cp $tmpid:/app/_logs/. ./_logs
+docker cp $tmpid:/app/sashimi/logs/. ./sashimi/logs
